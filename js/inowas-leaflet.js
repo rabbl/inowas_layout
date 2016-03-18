486e3a235121be95f3c232b59e054c3c8b7db975
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-03-18 15:54:33 +0100

    Further improvement in css

--- js/inowas-leaflet.js
+++ js/inowas-leaflet.js
@@ -29,3 +29,43 @@ function onMapClick(e) {
 
 mymap.on('click', onMapClick);
 
+
+// initialize the map on the "map" div with a given center and zoom
+var soilmodelmap = L.map('soil-model-map').setView(
+    [51.505, -0.065], 13
+);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(soilmodelmap);
+
+L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(soilmodelmap).bindPopup("I am a polygon.");
+
+
+// initialize the map on the "map" div with a given center and zoom
+var meshmap = L.map('mesh-map').setView(
+    [51.505, -0.065], 13
+);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(meshmap);
+
+L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(meshmap).bindPopup("I am a polygon.");
+
